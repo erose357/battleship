55d055eea1b762a753a6a9fd8dab8d98dad21cdc
erose357 <erose357@gmail.com>
2017-05-20 17:15:47 -0600

    initial commit - first game board and test file

---
+++ lib/game_board.rb
@@ -0,0 +1,46 @@
+class GameBoard
+
+  def print_board
+    print "#{top_border}\n"
+    print "#{line_one}\n"
+    print "#{line_two}\n"
+    print "#{line_three}\n"
+    print "#{line_four}\n"
+    print "#{line_five}\n"
+    print "#{bottom_border}\n"
+  end
+
+  def top_border
+    "==========="
+  end
+
+  def line_one
+    [ ".", "1", "2", "3", "4" ].join("\s")
+  end
+
+  def line_two
+    [ "A", ".", ".", ".", "." ].join("\s")
+  end
+
+  def line_three
+    [ "B", ".", ".", ".", "." ].join("\s")
+  end
+
+  def line_four
+    [ "C", ".", ".", ".", "." ].join("\s")
+  end
+
+  def line_five
+    [ "D", ".", ".", ".", "." ].join("\s")
+  end
+
+  def bottom_border
+    "==========="
+  end
+
+
+
+
+
+
+end
---
+++ test/game_board_test.rb
@@ -0,0 +1,71 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game_board.rb'
+require 'pry'
+class GameBoardTest < Minitest::Test
+
+  def test_instance_of
+
+    assert_instance_of GameBoard, GameBoard.new
+  end
+
+  def test_board_top_border
+    game_board = GameBoard.new
+    expected = "==========="
+    actual = game_board.top_border
+
+    assert_equal expected, actual
+  end
+
+  def test_print_line_one
+    game_board = GameBoard.new
+    expected = ".\s" "1\s" "2\s" "3\s" "4"
+
+    assert_equal expected, game_board.line_one
+  end
+
+  def test_print_line_two
+    game_board = GameBoard.new
+    expected = "A\s" ".\s" ".\s" ".\s" "."
+
+    assert_equal expected, game_board.line_two
+  end
+
+  def test_print_line_three
+    game_board = GameBoard.new
+    expected = "B\s" ".\s" ".\s" ".\s" "."
+
+    assert_equal expected, game_board.line_three
+  end
+
+  def test_print_line_four
+    game_board = GameBoard.new
+    expected = "C\s" ".\s" ".\s" ".\s" "."
+
+    assert_equal expected, game_board.line_four
+  end
+
+  def test_print_line_five
+    game_board = GameBoard.new
+    expected = "D\s" ".\s" ".\s" ".\s" "."
+
+    assert_equal expected, game_board.line_five
+  end
+
+  def test_print_bottom_border
+    game_board = GameBoard.new
+    expected = "==========="
+
+    assert_equal expected, game_board.bottom_border
+  end
+
+  def test_print_lines_one_and_two
+    game_board = GameBoard.new
+    expected = "===========\n"".\s" "1\s" "2\s" "3\s" "4\n""A\s" ".\s" ".\s" ".\s" ".\n""B" "." "." "." ".\n""C" "." "." "." ".\n""D" "." "." "." ".\n""===========\n"
+    actual = game_board.print_board
+
+    assert_equal expected, actual
+  end
+
+end
