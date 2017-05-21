e54837987847251fff2f52f8f382a1a1d570ef1c
erose357 <erose357@gmail.com>
2017-05-21 16:27:05 -0600

    add data structure and test files

---
+++ lib/data_structure.rb
@@ -0,0 +1,18 @@
+class DataStructure
+
+  def coordinates
+    { "A1" => "", "A2" => "", "A3" => "", "A4" => "",
+      "B1" => "", "B2" => "", "B3" => "", "B4" => "",
+      "C1" => "", "C2" => "", "C3" => "", "C4" => "",
+      "D1" => "", "D2" => "", "D3" => "", "D4" => "" }
+  end
+
+  def hit(spot)
+    coordinates[spot] = "H"
+  end
+
+  def miss(spot)
+    coordinates[spot] = "M"
+  end
+
+end
---
+++ test/data_structure_test.rb
@@ -0,0 +1,37 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/data_structure'
+
+class DataStructureTest < Minitest::Test
+
+  def test_keys_in_coordinates
+    data = DataStructure.new
+    expected = [ "A1", "A2", "A3", "A4", "B1", "B2", "B3",
+                 "B4", "C1", "C2", "C3", "C4", "D1", "D2",
+                 "D3", "D4" ]
+
+    assert_equal expected, data.coordinates.keys
+  end
+
+  def test_one_change_one_key_value
+    data = DataStructure.new
+    expected = "M"
+    actual = data.coordinates["A1"] = "M"
+
+    assert_equal expected, actual
+  end
+
+  def test_value_change_by_method
+    data = DataStructure.new
+
+    assert_equal "H", data.hit("C3")
+  end
+
+  def test_value_change_for_miss
+    data = DataStructure.new
+
+    assert_equal "M", data.miss("D4")
+  end
+
+end
